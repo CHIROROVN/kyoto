d32273acf6f28b01fe37a4e5df70b318cdce5e7f
dophu17 <dophu17@gmail.com>
2016-06-17 13:44:59 +0700

    update

--- storage/framework/views/dcdb47f37ce24249bc7332e332b92b00fb04b545.php
+++
@@ -1,5 +0,0 @@
-<?php echo $__env->make('frontend.element.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-	<!-- content -->
-      <?php echo $__env->yieldContent('content'); ?>
-    <!-- end content -->
-<?php echo $__env->make('frontend.element.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
